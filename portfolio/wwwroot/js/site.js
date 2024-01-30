7ece7d7fed657b2a9c3c1d5a44cbb5a247fadb68
JESSE <dtrsjesse121@gmail.com>
2024-01-30 06:52:37 -0500

    added contact links, bio, and experience sections

--- portfolio/wwwroot/js/site.js
+++ portfolio/wwwroot/js/site.js
@@ -2,5 +2,4 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
-import { showDialog } from 'https://codepen.io/bramus/pen/ZEqMOLz/cccfe67c2b9cdfbeb5fb59083dbd0a64.js';
-showDialog('https://scroll-driven-animations.style/demos/stacking-cards/css/');
+
